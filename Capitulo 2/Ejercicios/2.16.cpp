cd8f4ab279a3de74593dc27350a2db519c069dea
AngelFernandoPacsiPonce <angel.pacsi@ucsp.edu.pe>
2024-03-21 08:28:32 -0500

    Cap 3

---
+++ Capitulo 2/Ejercicios/2.16.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n1,n2;
+
+    cout<<"Por favor, introduzca 2 numeros: "<<endl;
+
+    cin>>n1>>n2;
+
+    return 0;
+}
---
+++ Capitulo 3/Ejercicios3/Account/Account.h
@@ -0,0 +1,15 @@
+#include <string>
+
+class Account{
+
+    public:
+        void setName(std::string accountName){
+            name=accountName;
+        }
+        std::string getName()const{
+            return name;
+        }
+    private:
+        std::string name;
+    
+};
